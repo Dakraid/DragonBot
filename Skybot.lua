d7e95894691f0c837a9a9d83641bc96b5f7a5cf2
Dakraid <dakraid@digital-dragons.de>
2017-08-17 15:27:04 +0200

    First changes to the way Factoids work and how it is integrated into the bot. Tokens are now gone and instead everything is done through patterns. Non functional as of yet.
    Signed-off-by: Dakraid <dakraid@digital-dragons.de>

--- Skybot.lua
+++ Skybot.lua
@@ -1,5 +1,5 @@
 local config    = require('config')
-local debugmode = GetConfig("debugmode")
+local debugmode = GetMainConfig("debugmode")
 
 _G.sqlite       = require('sqlite3')
 local discordia = require('discordia')
@@ -19,37 +19,18 @@ client:on('messageCreate', function(message)
   if message.author == client.user then return end
   
   -- Process the message and store the results
-  local out_msg, out_athr, out_rpt = prcs.ProcessMessage(message.content,message.author,message.member,message.guild)
+  local out_msg, out_athr = prcs.ProcessMessage(message.content,message.author)
   
   -- Check that out_msg is not empty
   if out_msg then
     -- If the return from the process includes a matching username PM the user
     if message.author.username == out_athr then
-      if out_rpt then
-        local count = table.getn(out_msg[1])
-        local split = math.floor(count / 2)
-        message.author:sendMessage(MultiMessage(out_msg[1],1,split), out_athr)
-        message.author:sendMessage(MultiMessage(out_msg[1],split,count), out_athr)
-      else
-        message.author:sendMessage(out_msg, out_athr)
-      end
+      message.author:sendMessage(out_msg, out_athr)
     else
       message.channel:sendMessage(out_msg)
     end
   end
 end)
-
-function MultiMessage(content,start,length)
-  local split_msg = ""
-  for i=start,length do
-    if i == length then
-      split_msg = split_msg .. content[i]
-    else
-      split_msg = split_msg .. content[i] .. ", "
-    end
-  end
-  return split_msg
-end
 
 -- Debugging Interface
 if debugmode == true then
@@ -58,24 +39,9 @@ if debugmode == true then
     io.write("Please enter input: ")
     io.flush()
     local input = io.read()
-    local out_msg, out_athr, out_rpt = prcs.ProcessMessage(input,TestUser)
-    if out_rpt then
-      local count = table.getn(out_msg[1])
-      local first = math.floor(count / 2)
-      local split_msg = ""
-      for i=1,first do
-          split_msg = split_msg .. out_msg[1][i] .. ", "
-      end
-      print("DEBUG: ",split_msg,out_athr)
-      split_msg = ""
-      for i=first,count do
-          split_msg = split_msg .. out_msg[1][i] .. ", "
-      end
-      print("OUTPUT: ",split_msg,out_athr)
-    else
-      print("OUTPUT: ",out_msg,out_athr)
-    end
+    local out_msg, out_athr = prcs.ProcessMessage(input,TestUser)
+    print("OUTPUT: ",out_msg,out_athr)
   until input == "quit"
 end
 
-client:run(config["token"])
+client:run(GetMainConfig("token"))
--- lua/netlib/process.lua
+++ lua/netlib/process.lua
@@ -1,7 +1,7 @@
 local public      = {}
 
 local Config      = require('config')
-local Trigger     = GetConfig("trigger")
+local Trigger     = GetMainConfig("trigger")
 
 local Commands    = require('./commands')
 
@@ -11,58 +11,21 @@ local out_message, out_author, out_repeat
 local Permissions = require('./permissions')
 local Factoids    = require('./factoids')
 
-function public.ProcessMessage(content,user,member,guild)
+function public.ProcessMessage(content,user)
+  -- Abort if either content or user is empty
   if not content then return end
   if not user then return end
+  
+  -- Check if the trigger is at the expected position and if there is an actual input
   if content:find(Trigger) == 1 and content:len() > 1 then
-    Tokens = Tokenize(content)
-    if not Permissions.CheckBlacklist(user) then
-      if Permissions.CheckPermission(user,0) then
-        if not CheckCommand(Tokens[1]) then
-          out_message, out_author, out_repeat = Factoids.Process(Tokens,user)
-        else
-          out_message, out_author, out_repeat = ProcessCommand(user,member,guild,Tokens)
-        end
-      end
+    if not Permissions.CheckBlacklist(user) and Permissions.CheckPermission(user,0) then
+        Factoids.Process(content,user)
     end
     return out_message, out_author, out_repeat
   end
 end
 
-function ProcessCommand(user,member,guild,Tokens)
-  local command = Tokens[1]:lower()
-  out_author  = user.username
-  if command     == Trigger .. "help" then
-    out_message = ": Enter facts: \"!x is y\"; Replace facts: \"!no x is y\"; Delete facts: \"!forget x\";"
-  end
-  if Permissions.CheckPermission(user,1) then
-    if command     == Trigger .. "forget" then
-      out_message   = Factoids.FactRemove(Tokens,Trigger)
-    elseif command == Trigger .. "no" then
-      out_message   = Factoids.FactReplace(Tokens,Trigger,user)
-    end
-  end
-  if Permissions.CheckPermission(user,2) then
-    if     command == Trigger .. "connect" then
-      out_message   = Factoids.Connect()
-    elseif command == Trigger .. "reconnect" then
-      out_message   = Factoids.Reconnect()
-    elseif command == Trigger .. "disconnect" then
-      out_message   = Factoids.Disconnect()
-    elseif command == Trigger .. "lockdb" then
-      out_message   = Factoids.LockDatabase()
-    elseif command == Trigger .. "unlockdb" then
-      out_message   = Factoids.UnlockDatabase()
-    elseif command == Trigger .. "listkeys" then
-      out_message   = Factoids.ListKeys()
-      out_repeat    = true
-    end
-  end
-  if Permissions.CheckPermission(user,3) then
-    if     command == Trigger .. "quit" then
-      Quit()
-    end
-  end
+function ProcessCommand(content,user)
   return out_message, out_author, out_repeat
 end
 
@@ -70,30 +33,9 @@ end
     Utility Functions
 --]]
 
-function Tokenize(content)
-  local tkns = {}
-  local cnt  = 0
-  content = content:gsub("^(!%a+)%s*,", "%1 ", 1)
-  for token in string.gmatch(content, "%S+") do
-    cnt       = cnt + 1
-    tkns[cnt] = token
-  end
-  return tkns
-end
-
 function Quit()
   Factoids.Disconnect()
   os.exit()
-end
-
--- Returns true if the command has been found in the list
-function CheckCommand(content)
-  local tmp = content:sub(2)
-  if table.find(Commands,tmp) then
-    return true
-  else
-    return false
-  end
 end
 
 return public
